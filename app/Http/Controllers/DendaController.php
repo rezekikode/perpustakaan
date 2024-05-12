34f28c61554e055c6772332132b445f236c56e4f
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-05-13 05:21:33 +0800

    CHANGE

---
+++ app/Http/Controllers/DendaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Denda;
+use App\Http\Requests\StoreDendaRequest;
+use App\Http\Requests\UpdateDendaRequest;
+
+class DendaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDendaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Denda $denda)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Denda $denda)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDendaRequest $request, Denda $denda)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Denda $denda)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PenerbitController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Penerbit;
+use App\Http\Requests\StorePenerbitRequest;
+use App\Http\Requests\UpdatePenerbitRequest;
+
+class PenerbitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePenerbitRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Penerbit $penerbit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Penerbit $penerbit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePenerbitRequest $request, Penerbit $penerbit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Penerbit $penerbit)
+    {
+        //
+    }
+}
